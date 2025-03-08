90f20bb1fdc2c2fea0dd71c745d5477a502e0d17
Calvin Lee <lclee3@cafe-jg.stanford.edu>
2025-03-07 16:48:40 -0800

    yichen changes

--- fv/synthlc/env.sh
+++ fv/synthlc/env.sh
@@ -1,7 +1,6 @@
-setenv CLK clock
-setenv TOPMOD Rocket
-setenv RESET reset
+export CLK="clock"
+export TOPMOD="Rocket"
+export RESET="reset"
 cp src/HB_template.py.template src/HB_template.py
 sed -i "s~RESET~${RESET}~" src/HB_template.py
 sed -i "s~CLK~${CLK}~" src/HB_template.py
-
