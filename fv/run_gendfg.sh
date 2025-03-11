16083d538aacce2deb11bacf1222e7a93844ae01
Calvin Lee <calvin.lee.socialmedia@gmail.com>
2025-03-10 19:31:58 -0700

    changed gendfg

--- fv/run_gendfg.sh
+++ fv/run_gendfg.sh
@@ -6,19 +6,14 @@ python3 gen_dfg_all_pls.py gen
 touch tmp.sv 
 if [ $BYPASS -eq 1 ];
 then
-    cp expected_output/dfg_e.txt .
+    cp expected_output/dfg_e.0 .
 else
     cd ../../
     ./RUN_JG.sh -j synthlc/xGenPerfLocDfgDiv -s synthlc/xGenPerfLocDfgDiv/tmp.sv -t synthlc/xGenPerfLocDfgDiv/get_dfg.tcl -g 0 
     cd synthlc/xGenPerfLocDfgDiv
-    cd tmp_rundir
-    rm -rf tmp_jgsession*
-    cd ..
     mylog=$(ls tmp_rundir/*/jg.log)
     cp ${mylog} "get_dfg.tcl.log"
     python3 gen_dfg_all_pls.py pp
-    cat dfg_e.txt | sort -u > cur.txt 
-    cat expected_output/dfg_e.txt|sort -u > prev.txt    
     # To ensure the results are the same in case the edges are reordered 
     mv dfg_e.txt dfg_e.0
 fi 
